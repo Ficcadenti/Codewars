7367f65997e34759f721c580781da7eef82d29ec
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2020-03-05 15:02:04 +0100

    Easy line

---
+++ Easy Line/src/it/raffo/codewars/Easyline.java
@@ -0,0 +1,84 @@
+package it.raffo.codewars;
+
+import java.math.BigInteger;
+
+public class Easyline
+{
+
+	private static BigInteger choose(int n, int p)
+	{
+		BigInteger ret = BigInteger.ONE;
+		for (int i = 0; i < p; i++)
+		{
+			ret = ret.multiply(BigInteger.valueOf(n - i)).divide(BigInteger.valueOf(i + 1));
+		}
+		return ret;
+	}
+
+	public static BigInteger easyLine2(int n)
+	{
+		return choose(2 * n, n);
+	}
+
+	public static BigInteger easyLine(int n)
+	{
+		System.out.println(n);
+		BigInteger sumCoefBinomiali = BigInteger.valueOf(0);
+		BigInteger[][] tartaglia = creaTartaglia(n + 1);
+		// stampaTartaglia(tartaglia);
+		for (int i = 0; i < tartaglia[n].length; i++)
+		{
+			// BigInteger binomio = BigInteger.valueOf(tartaglia[n][i].longValue());
+			BigInteger binomio = tartaglia[n][i];
+			binomio = binomio.multiply(tartaglia[n][i]);
+			sumCoefBinomiali = sumCoefBinomiali.add(binomio);
+		}
+		return sumCoefBinomiali;
+	}
+
+	public static BigInteger[][] creaTartaglia(int n)
+	{
+
+		BigInteger[][] tartaglia; // o triangolo di Pascal
+		int i, j;
+		tartaglia = new BigInteger[n][];
+
+		for (i = 0; i < tartaglia.length; i++)
+		{
+			tartaglia[i] = new BigInteger[i + 1];
+		}
+
+		for (i = 0; i < tartaglia.length; i++)
+
+		{
+
+			tartaglia[i][0] = BigInteger.valueOf(1);
+			tartaglia[i][i] = BigInteger.valueOf(1);
+			for (j = 1; j < i; j++)
+			{
+				tartaglia[i][j] = tartaglia[i - 1][j - 1].add(tartaglia[i - 1][j]);
+
+				// BigInteger.valueOf(tartaglia[i - 1][j -
+				// 1].longValue()).add(BigInteger.valueOf(tartaglia[i - 1][j].longValue()));
+			}
+
+		}
+		return tartaglia;
+	}
+
+	public static void stampaTartaglia(BigInteger[][] tartaglia)
+	{
+		for (int i = 0; i < tartaglia.length; i++)
+
+		{
+
+			for (int j = 0; j < tartaglia[i].length; j++)
+			{
+				System.out.print(tartaglia[i][j] + " ");
+			}
+
+			System.out.println("");
+
+		}
+	}
+}
---
+++ Easy Line/src/it/raffo/codewars/Test.java
@@ -0,0 +1,13 @@
+package it.raffo.codewars;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		Easyline.stampaTartaglia(Easyline.creaTartaglia(68));
+		System.out.println(Easyline.easyLine(67));
+
+	}
+
+}
